0723bf944a8f2f11f1f9be414c8d23db2107c730
Umgoel <76695806+Umgoel@users.noreply.github.com>
2022-11-13 19:30:27 +0530

    Update l1.cpp

--- l1.cpp
+++ l1.cpp
@@ -1,14 +1,6 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include<bits/stdc++.h>
 #include<vector>
-// #include<iostream>
+#include<iostream>
 using namespace std;    
 void count_no_islands(vector<vector<int> > &M, int r, int c);
 void dfs(vector<vector<int> > &M, int i, int j, int r, int c);
